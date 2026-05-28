890656a902c7ef16f34672937ca64f7b287dfac0
Robin Ekblad <robinekbladh@hotmail.com>
2026-05-28 08:44:13 +0200

    set up markers

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,14 @@
 import pytest
 from calculator import add
 
-def test_add():
-    assert add(2, 3) == 5
+@pytest.mark.parametrize("a, b, expected", [
+    (3, 3, 6),
+    (5, 5, 20),
+    (15, 15, 30),
+    (100, 200, 300), 
+    (27, 34, 61)
+])
+
+def test_add(a, b, expected):
+    assert add(a, b) == expected
 
